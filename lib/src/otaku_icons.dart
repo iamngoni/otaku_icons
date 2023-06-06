18c07fa277b38418eda81f9e020ec43694b2f6aa
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-06-06 22:11:58 +0200

    feat: bubble icons

--- lib/src/otaku_icons.dart
+++ lib/src/otaku_icons.dart
@@ -1,7 +1,40 @@
-/// {@template otaku_icons}
-/// Modest Icons
-/// {@endtemplate}
+import 'package:flutter/widgets.dart';
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: constant_identifier_names
+@immutable
+class _OtakuIconsData extends IconData {
+  const _OtakuIconsData(int codePoint, this.name)
+      : super(
+          codePoint,
+          fontFamily: 'OtakuIcons',
+        );
+
+  final String name;
+}
+
+@immutable
 class OtakuIcons {
-  /// {@macro otaku_icons}
-  const OtakuIcons();
+  const OtakuIcons._();
+
+  static const battery_broken = _OtakuIconsData(0xf101, 'battery_broken');
+  static const battery_empty = _OtakuIconsData(0xf102, 'battery_empty');
+  static const bubble_bold = _OtakuIconsData(0xf103, 'bubble_bold');
+  static const bubble_broken = _OtakuIconsData(0xf104, 'bubble_broken');
+  static const bubble_bulk = _OtakuIconsData(0xf105, 'bubble_bulk');
+  static const bubble_linear = _OtakuIconsData(0xf106, 'bubble_linear');
+  static const bubble_outline = _OtakuIconsData(0xf107, 'bubble_outline');
+  static const bubble_twotone = _OtakuIconsData(0xf108, 'bubble_twotone');
+
+  static const all = <String, _OtakuIconsData>{
+    'battery_broken': battery_broken,
+    'battery_empty': battery_empty,
+    'bubble_bold': bubble_bold,
+    'bubble_broken': bubble_broken,
+    'bubble_bulk': bubble_bulk,
+    'bubble_linear': bubble_linear,
+    'bubble_outline': bubble_outline,
+    'bubble_twotone': bubble_twotone,
+  };
 }
