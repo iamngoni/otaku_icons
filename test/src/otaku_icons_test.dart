1a19a12d9914c60849afffa905b48b69bce43d8f
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-06-06 22:16:12 +0200

    feat: battery icons

--- test/src/otaku_icons_test.dart
+++ test/src/otaku_icons_test.dart
@@ -1,12 +1,3 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:otaku_icons/otaku_icons.dart';
-
-void main() {
-  group('OtakuIcons', () {
-    test('can be instantiated', () {
-      expect(OtakuIcons(), isNotNull);
-    });
-  });
-}
+void main() {}
